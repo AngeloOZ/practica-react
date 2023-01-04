6db9ec753396c6170a2556445ebae6105744ecd0
angelooz <angelo-mjz7@hotmail.com>
2023-01-04 11:54:02 -0500

    updade: dibujar componentes de array

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 import { Contador } from "./Components";
 import { useContador } from "./Hooks/useContador";
 
 function App() {
+  const showList = false;
   const { contador, handleClickAdd } = useContador();
   const { contador: cont, handleClickSub: fun2 } = useContador();
+  const [nombres, setNombres] = useState([
+    "Angello",
+    "Athina",
+    "Kathy1",
+    "Kathy2",
+    "David",
+    "Pato",
+  ]);
   return (
     <div className="App">
-      <Contador />
-      <Contador />
+      {/* <Contador />
+      <Contador /> */}
       {/* <Contador /> */}
 
-      <>
+      {/* <>
         <h1>{contador}</h1>
         <button onClick={handleClickAdd}>+</button>
       </>
       <>
         <h1>{cont}</h1>
         <button onClick={fun2}>-</button>
-      </>
+      </> */}
+      <p>Listado de oficina 2</p>
+      <ul>
+        {nombres.map((nombre, i) => (
+          <li key={i}>{nombre}</li>
+        ))}
+      </ul>
     </div>
   );
 }
